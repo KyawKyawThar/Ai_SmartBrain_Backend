47262f834298de2abdbd2bf31dd1e2b28673f465
KyawKyawThar <kkt@kyawkyawThar-MacBook-Pro.local>
2020-12-07 17:39:52 +0630

    Updating host

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1', //localhost
+    host: 'postgresql-adjacent-84048', //localhost
     user: 'kkt', //add your user name for the database here
     password: '', //add your correct password in here
     database: 'ai-smartbrain', //add your database name you created here
